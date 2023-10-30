1f3a87a741eeabcfa877889aaf29a3dda3cfea4e
Alejjaandro <alejandroolaso98@gmail.com>
2023-10-30 09:07:36 +0100

    before removing routes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { InjectTokenInterceptor } from '@core/interceptors/inject-token.interceptor';
+import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    // We only import other modules
+    providers: [
+        importProvidersFrom(
+        BrowserModule, AppRoutingModule),
+        CookieService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: InjectTokenInterceptor,
+            multi: true
+        },
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
